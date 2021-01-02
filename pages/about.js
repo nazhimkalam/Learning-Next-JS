31f76c527daf667ab7f3d857adf5a8a0aae6eba4
Nazhim <nazhimkalam@gmail.com>
2021-01-02 12:51:30 +0530

    Changing the head meta content

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 import Nav from '../components/Nav/Nav';
+import Head from "next/head"
 
 function about() {
 	return (
 		<div>
+			<head>
+				<title>About Page</title>
+                <meta name="description" content = "Learning Next Js"/>
+			</head>
 			<Nav />
 			<h3>This is the about page</h3>
 			<Link href="/">
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Link from 'next/link';
 import Nav from '../components/Nav/Nav';
+import Head from 'next/head';
 
 function index() {
 	return (
 		<div>
-            <Nav />
+			<head>
+				<title>Home Page</title>
+				<meta name="description" content="Learning Next Js" />
+			</head>
+			<Nav />
 			<h2>Hello World!</h2>
 			<Link href="/about">
 				<a style={myStyle}>About Page</a>
